ead7d573d4619a78d60dbbe70bb43ceec0a647ff
Igor Araujo <140194982+igoredw@users.noreply.github.com>
2023-10-09 15:04:51 -0300

    Add files via upload

---
+++ Projeto inicial C#/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string nomeDigitado;
+        int idadeDigitada;
+
+        Console.WriteLine(" Digite seu nome");
+        nomeDigitado = Console.ReadLine();
+
+
+        Console.WriteLine("idade: ");
+        idadeDigitada = Convert.ToInt32(Console.ReadLine());
+
+        if (idadeDigitada < 18)
+        {
+            Console.WriteLine("Você é menor de idade");
+        }
+        else
+        {
+            Console.WriteLine("Você é maior de idade");
+        }
+    }
+}
